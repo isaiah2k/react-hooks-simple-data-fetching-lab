59b9bbed16712f7d62a837e5edc4eb9f088e054b
IsaiahMitchell <zaymgmt707@gmail.com>
2025-04-10 18:16:06 -0700

    updates app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
-// create your App component here
+// src/components/App.js
+import React, { useState, useEffect } from "react"
+
+function App() {
+  const [imageUrl, setImageUrl] = useState("")
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((r) => r.json())
+      .then((data) => {
+        setImageUrl(data.message)
+        setIsLoading(false)
+      })
+  }, [])
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={imageUrl} alt="A Random Dog" />
+      )}
+    </div>
+  )
+}
+
+export default App
